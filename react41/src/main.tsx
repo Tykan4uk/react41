0ace32d9ad7c8e44ff4a9eb4efbc7ec162caa0ae
vasyliev.v <vasyl.vasyliev@cyper.ca>
2023-05-21 19:19:17 +0300

    Homework

--- react41/src/main.tsx
+++ react41/src/main.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Form } from './components/form/form'
+import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Form />
+    <App />
   </React.StrictMode>,
 )
